098bafb4ee9c2f610a71981fa0fd48cf63a02b3d
Oscar te Giffel <oscar@tegiffel.com>
2023-12-03 23:59:45 +0100

    feat: removed unused dependencies and added day 1 part 1

--- 2023/rust/day-01/src/bin/part1.rs
+++ 2023/rust/day-01/src/bin/part1.rs
@@ -1,16 +1,8 @@
 use day_01::part1::process;
 use miette::Context;
 
-#[cfg(feature = "dhat-heap")]
-#[global_allocator]
-static ALLOC: dhat::Alloc = dhat::Alloc;
-
 #[tracing::instrument]
 fn main() -> miette::Result<()> {
-    #[cfg(feature = "dhat-heap")]
-    let _profiler = dhat::Profiler::new_heap();
-
-    #[cfg(not(feature = "dhat-heap"))]
     tracing_subscriber::fmt::init();
 
     let file = include_str!("../../input1.txt");
@@ -18,3 +10,4 @@ fn main() -> miette::Result<()> {
     println!("{}", result);
     Ok(())
 }
+
--- 2023/rust/day-01/src/part1.rs
+++ 2023/rust/day-01/src/part1.rs
@@ -1,19 +1,23 @@
 use crate::custom_error::AocError;
 
+pub fn process_line(line: &str) -> u32 {
+    let digits = line
+        .chars()
+        .filter(|c| c.is_ascii_digit())
+        .collect::<Vec<_>>();
+
+    let first = digits.first().unwrap();
+    let last = digits.last().unwrap();
+
+    let d = format!("{}{}", first, last);
+
+    d.parse::<u32>().unwrap()
+}
+
 #[tracing::instrument]
 pub fn process(input: &str) -> miette::Result<String, AocError> {
     let result = input.lines().fold(0, |acc, line| {
-        let digits = line
-            .chars()
-            .filter(|c| c.is_ascii_digit())
-            .collect::<Vec<_>>();
-
-        let first = digits.first().unwrap();
-        let last = digits.last().unwrap();
-
-        let d = format!("{}{}", first, last);
-
-        let digit = d.parse::<u32>().unwrap();
+        let digit = process_line(line);
 
         acc + digit
     });
@@ -24,6 +28,20 @@ pub fn process(input: &str) -> miette::Result<String, AocError> {
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    use rstest::rstest;
+
+    #[rstest]
+    #[case("two1nine", 29)]
+    #[case("eightwothree", 83)]
+    #[case("abcone2threexyz", 13)]
+    #[case("xtwone3four", 24)]
+    #[case("4nineeightseven2", 42)]
+    #[case("zoneight234", 14)]
+    #[case("7pqrstsixteen", 76)]
+    fn line_test(#[case] line: &str, #[case] expected: u32) {
+        // assert_eq!(expected, process_line(line))
+    }
 
     #[test]
     fn test_process() -> miette::Result<()> {
